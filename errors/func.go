c38e19d872b40c9ba50586a2fd159e9e8e3155cb
Eric Hackborn <hackborn@gmail.com>
2024-01-30 15:19:20 -0800

    Add fatal error convenience

--- errors/func.go
+++ errors/func.go
@@ -1,5 +1,9 @@
 package errors
 
+import (
+	"log"
+)
+
 // First answers the first non-nil error in the list.
 func First(errs ...error) error {
 	for _, err := range errs {
@@ -15,6 +19,15 @@ func Panic(errs ...error) {
 	for _, err := range errs {
 		if err != nil {
 			panic(err)
+		}
+	}
+}
+
+// LogFatal does a log.Fata if any error is non-nil.
+func LogFatal(errs ...error) {
+	for _, err := range errs {
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
 }
