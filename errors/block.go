cc8b2075637a8a21544b65f18063f5969cab649e
Eric Hackborn <hackborn@gmail.com>
2024-02-08 11:54:14 -0800

    errors: add HasError to Block

--- errors/block.go
+++ errors/block.go
@@ -1,9 +1,12 @@
 package errors
 
+// Block tracks one or more errors.
 type Block interface {
 	AddError(error)
+	HasError() bool
 }
 
+// FirstBlock is a Block that stores the first error it receives.
 type FirstBlock struct {
 	Err error
 }
@@ -14,8 +17,17 @@ func (c *FirstBlock) AddError(e error) {
 	}
 }
 
+func (c *FirstBlock) HasError() bool {
+	return c.Err != nil
+}
+
+// NullBlock is an empty Block that doesn't store errors.
 type NullBlock struct {
 }
 
 func (c *NullBlock) AddError(e error) {
+}
+
+func (c *NullBlock) HasError() bool {
+	return false
 }
