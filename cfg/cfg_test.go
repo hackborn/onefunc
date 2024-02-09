9d95e2ba79dcb89512698f377595b1d581b886b7
Eric Hackborn <hackborn@gmail.com>
2024-02-09 11:20:53 -0800

    cfg: Add env vars to settings

--- cfg/cfg_test.go
+++ cfg/cfg_test.go
@@ -2,10 +2,17 @@ package cfg
 
 import (
 	"embed"
+	"os"
 	"testing"
 
 	"github.com/hackborn/onefunc/jacl"
 )
+
+func TestMain(m *testing.M) {
+	setupTests()
+	code := m.Run()
+	os.Exit(code)
+}
 
 // ---------------------------------------------------------
 // TEST-STRING
@@ -21,6 +28,8 @@ func TestString(t *testing.T) {
 		{[]Option{WithFS(dataFs, "test_data/settings_a.json")}, nil, "a", "anna", nil},
 		{[]Option{WithFS(dataFs, "test_data/settings_b.json")}, nil, "a", "ava", nil},
 		{[]Option{WithFS(dataFs, "test_data/settings_[{a-b}].json")}, nil, "a", "ava", nil},
+		{[]Option{WithEnv(EnvPattern("CFG_TESTDATA_*"))}, nil, "CFG_TESTDATA_A", "ant", nil},
+		{[]Option{WithEnv(EnvPrefix("CFG_TESTDATA_"))}, nil, "A", "ant", nil},
 	}
 	for i, v := range table {
 		s, haveErr := NewSettings(v.opts...)
@@ -34,6 +43,15 @@ func TestString(t *testing.T) {
 			t.Fatalf("TestString %v has \"%v\" but wants \"%v\"", i, have, v.want)
 		}
 	}
+}
+
+// ---------------------------------------------------------
+// LIFECYCLE
+
+func setupTests() {
+	os.Setenv("CFG_TESTDATA_A", "ant")
+	os.Setenv("CFG_TESTDATA_B", "bear")
+	os.Setenv("CFG_TESTDATA_C", "cat")
 }
 
 // ---------------------------------------------------------
--- cfg/opt.go
+++ cfg/opt.go
@@ -3,18 +3,21 @@ package cfg
 import (
 	"encoding/json"
 	"io/fs"
+	"os"
+	"path"
+	"strings"
 
 	oferrors "github.com/hackborn/onefunc/errors"
 )
 
 type Option func(Settings, oferrors.Block)
 
-// WithFS loads all files that match the glob pattern
+// WithFS loads all files that match the pattern
 // into the Settings. All matched files must be in JSON format.
-// See fs.Glob() for glob rules.
-func WithFS(fsys fs.FS, glob string) Option {
+// See path.Match() for match rules.
+func WithFS(fsys fs.FS, pattern string) Option {
 	return func(s Settings, eb oferrors.Block) {
-		matches, err := fs.Glob(fsys, glob)
+		matches, err := fs.Glob(fsys, pattern)
 		eb.AddError(err)
 
 		for _, match := range matches {
@@ -28,4 +31,71 @@ func WithFS(fsys fs.FS, glob string) Option {
 			mergeKeys(s.t, s2.t)
 		}
 	}
+}
+
+// WithEnv loads all matching env vars.
+func WithEnv(match EnvMatcher) Option {
+	return func(s Settings, eb oferrors.Block) {
+		envs := os.Environ()
+		s2 := emptySettings(s.rw)
+		for _, env := range envs {
+			pos := strings.Index(env, "=")
+			if pos > 0 && pos < len(env)-1 {
+				left := env[0:pos]
+				right := env[pos+1:]
+				left, err := match.Match(left)
+				eb.AddError(err)
+				if left != "" {
+					s2.t[left] = right
+				}
+			}
+		}
+		mergeKeys(s.t, s2.t)
+	}
+}
+
+// EnvMatcher is used by WithEnv to accept env vars and
+// potentially modify the name.
+type EnvMatcher interface {
+	// Answer a key if a match, or else "". The key
+	// might be the same key in the args, or it might be modified.
+	Match(key string) (string, error)
+}
+
+// EnvPattern loads all env vars that match the pattern
+// into the Settings. See path.Match() for match rules.
+func EnvPattern(pattern string) EnvMatcher {
+	return &envPatternMatcher{pattern: pattern}
+}
+
+// EnvPrefix loads all env vars that match the prefix
+// into the Settings. The prefix will be stripped.
+func EnvPrefix(prefix string) EnvMatcher {
+	return &envPrefixMatcher{prefix: prefix}
+}
+
+type envPatternMatcher struct {
+	pattern string
+}
+
+func (m *envPatternMatcher) Match(key string) (string, error) {
+	ok, err := path.Match(m.pattern, key)
+	if err != nil {
+		return "", err
+	}
+	if !ok {
+		return "", nil
+	}
+	return key, nil
+}
+
+type envPrefixMatcher struct {
+	prefix string
+}
+
+func (m *envPrefixMatcher) Match(key string) (string, error) {
+	if !strings.HasPrefix(key, m.prefix) {
+		return "", nil
+	}
+	return key[len(m.prefix):], nil
 }
