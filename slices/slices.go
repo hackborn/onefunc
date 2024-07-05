8a233f5db0003ca1bf0b2c03234bbbaafd49c0d8
Eric Hackborn <hackborn@gmail.com>
2024-07-05 14:28:37 -0700

    slices: Add a setsize func.

--- slices/slices.go
+++ slices/slices.go
@@ -4,10 +4,10 @@ package slices
 // a new slice based on evaluating the function for
 // each member of the incoming slice.
 // Example:
-// 	return ArrayFrom(d.Fields, func(f structField) string {
+//
+//	return ArrayFrom(d.Fields, func(f structField) string {
 //		return f.Field
 //	})
-
 func ArrayFrom[T any, U any](s []T, f func(T) U) []U {
 	if len(s) < 1 {
 		return []U{}
@@ -17,4 +17,14 @@ func ArrayFrom[T any, U any](s []T, f func(T) U) []U {
 		result[i] = f(v)
 	}
 	return result
+}
+
+// SetLen returns a slice with the given length,
+// reusing the supplied slice if it has enough capacity.
+func SetLen[T any](s []T, area int) []T {
+	if cap(s) < area {
+		return make([]T, area, area)
+	} else {
+		return s[:area]
+	}
 }
